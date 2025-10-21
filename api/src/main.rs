ecbec0284bc2903e7f864fc8f7cf8d145df886ce
sajeevan <sajeevanjspy@gmail.com>
2025-10-21 10:23:50 +0530

    feat: add structured telemetry system with tracing subscriber setup - added modular telemetry architecture under src/telemetry/ - integrated tracing subscriber with layered setup - initialized telemetry in main application startup

--- api/src/main.rs
+++ api/src/main.rs
@@ -7,13 +7,16 @@ use actix_web::{App, HttpServer};
 
 mod config;
 mod routes;
+mod telemetry;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = config::Config::from_env();
 
-    println!("Starting server at http://{}", config.addr());
+    // telemetry
+    telemetry::init();
 
+    tracing::info!("Starting server at http://{}", config.addr());
     HttpServer::new(move || App::new().service(routes::health::health_check))
         .bind(config.addr())?
         .run()
---
+++ api/src/telemetry/mod.rs
@@ -0,0 +1,17 @@
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
+const DEFAULT_TRACE_LEVEL: &'static str = "trace";
+
+pub fn init() {
+    // use `RUST_LOG` if available, otherwise default to `TRACE`
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(DEFAULT_TRACE_LEVEL));
+
+    let fmt_layer = fmt::layer().with_target(true).with_level(true).compact();
+    tracing_subscriber::registry()
+        .with(env_filter)
+        .with(fmt_layer)
+        .init();
+
+    tracing::info!("telemetry initialized");
+}
