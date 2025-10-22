e0bdb5bb4b4027c51ac235362ee6c7071c9ed50f
sajeevan <sajeevanjspy@gmail.com>
2025-10-22 22:01:56 +0530

    feat: add websocket client to connect with API server - implemented client-side websocket connection to backend - sends a simple ping message after connection

--- playground/public/script.js
+++ playground/public/script.js
@@ -1,6 +1,32 @@
-const SERVER_IP = "http://localhost:8080";
+const SERVER_IP = 'http://localhost:8080';
+const WEBSOCKET = 'ws://localhost:8080/ws';
 
 const health = document.getElementById('health')
+
+function ws_connection() {
+    try {
+        const socket = new WebSocket(WEBSOCKET);
+
+        socket.addEventListener('open', event => {
+            console.log('Websocket connection opened:', event);
+            socket.send("ping");
+        })
+
+        socket.addEventListener('error', e => {
+            console.error("webSocket error:", e);
+        })
+
+        socket.addEventListener('message', event => {
+            console.log("message received from server:", event.data);
+        })
+
+        socket.addEventListener('close', event => {
+            console.log("webSocket connection closed:", event);
+        })
+    } catch (err) {
+        console.log(`failed to connect to the websocket ${err}`)
+    }
+}
 
 async function health_check() {
     try {
@@ -9,14 +35,18 @@ async function health_check() {
             const data = await response.json();
             if (data.status == "ok") {
                 health.innerHTML = 'server is running'
+                return
             }
-        } else {
-            health.innerHTML = 'server is stopped'
         }
+        throw new Error('failed to connect to the server');
     } catch (err) {
-        health.innerHTML = 'server is stopped'
+        health.innerHTML = 'cannot reach the server'
     }
 }
 
 health_check()
+    .then(() => {
+        // if the health request is ok, then try to connect it to the websocket
+        ws_connection();
+    })
     .catch(err => console.error(err))
