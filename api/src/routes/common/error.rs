94ecbb2fe545ad2d40a7b11cd3faf3a42b9e2aa6
sajeevan <sajeevanjspy@gmail.com>
2025-10-28 19:20:26 +0530

    feat: introduce ApiError for unified error handling - added ApiError enum to standardize error management across the API - integrated 	hiserror for ergonomic and maintainable error definitions - currently supports sqlx::Error variant for database-related errors - improves consistency and simplifies Actix Web response generation

---
+++ api/src/routes/common/error.rs
@@ -0,0 +1,24 @@
+use actix_web::{HttpResponse, ResponseError};
+
+use super::ApiResult;
+
+#[derive(Debug, thiserror::Error)]
+#[non_exhaustive]
+pub enum ApiError {
+    #[error("Database connection failed: {0}")]
+    DatabaseError(#[from] sqlx::Error),
+}
+
+impl ResponseError for ApiError {
+    fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
+        use ApiError::*;
+
+        match self {
+            DatabaseError(err) => {
+                tracing::error!(error = %err);
+                HttpResponse::InternalServerError()
+                    .json(ApiResult::<()>::error("Internal server error"))
+            }
+        }
+    }
+}
--- api/src/routes/common/mod.rs
+++ api/src/routes/common/mod.rs
@@ -1,4 +1,6 @@
+mod error;
 mod response;
 pub mod time;
 
+pub use error::ApiError;
 pub use response::{ApiResult, PaginatedResult, PaginationQuery};
