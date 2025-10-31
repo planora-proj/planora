19c080c78a4cd7adfbda48afe746bdcc92100287
sajeevan <sajeevanjspy@gmail.com>
2025-10-31 10:48:20 +0530

    feat: implement signup route under auth scope - generate session key upon successful signup - register route under /auth scope

--- api/src/routes/v1/auth/mod.rs
+++ api/src/routes/v1/auth/mod.rs
@@ -1,5 +1,7 @@
 use actix_web::Scope;
 
+mod signup;
+
 pub fn auth_scope() -> Scope {
-    Scope::new("/auth")
+    Scope::new("/auth").service(signup::signup)
 }
---
+++ api/src/routes/v1/auth/signup.rs
@@ -0,0 +1,70 @@
+use actix_web::{
+    HttpResponse, Responder,
+    cookie::{Cookie, SameSite},
+    post, web,
+};
+
+use crate::{
+    db::{manager::DbManager, models::User, repos::UserRepo},
+    routes::common::{ApiError, ApiResult},
+    services::JwtService,
+};
+
+#[cfg_attr(test, derive(serde::Serialize))]
+#[derive(serde::Deserialize)]
+struct SignupPayload {
+    pub username: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[post("/signup")]
+async fn signup(
+    manager: web::Data<DbManager>,
+    jwt_service: web::Data<JwtService>,
+    payload: web::Json<SignupPayload>,
+) -> Result<impl Responder, ApiError> {
+    let username = payload.username.clone();
+    let email = payload.email.clone();
+    let password = payload.password.clone();
+
+    let pool = manager.get_pool("planora").await.unwrap();
+
+    // verify is any other user with the same email
+    let user_repo = UserRepo::new(&pool);
+    let u = user_repo
+        .find_by_email(email.clone())
+        .await
+        .map_err(ApiError::from)?;
+
+    if u.is_some() {
+        return Ok(HttpResponse::NotAcceptable()
+            .json(ApiResult::<()>::error("email is already registered")));
+    }
+
+    // create user
+    let inserted_user = user_repo
+        .create_user(&User {
+            username,
+            email: email.clone(),
+            password: Some(password),
+            ..Default::default()
+        })
+        .await
+        .map_err(ApiError::from)?;
+    tracing::info!("user has been created: {email}");
+
+    // session creation
+    let (access_token, _refresh_token) = jwt_service.generate_tokens(inserted_user.user_id)?;
+
+    let cookie = Cookie::build(JwtService::JWT_SESSION_KEY, access_token)
+        .path("/")
+        .secure(false)
+        .http_only(true)
+        .same_site(SameSite::None)
+        .finish();
+
+    Ok(HttpResponse::Ok()
+        .cookie(cookie)
+        .body("logged in successfully!"))
+}
