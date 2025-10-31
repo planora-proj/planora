b2261fe605b80fbe83fcf8c60467946fd87694f3
sajeevan <sajeevanjspy@gmail.com>
2025-10-31 10:02:48 +0530

    feat: add Clone and Default derives to User model for easier initialization and data copying

--- api/src/db/models/user.rs
+++ api/src/db/models/user.rs
@@ -2,7 +2,7 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
+#[derive(Debug, Default, Clone, Serialize, Deserialize, sqlx::FromRow)]
 pub struct User {
     pub user_id: Uuid,
     pub user_tag: Option<String>,
