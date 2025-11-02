910de87fa9c0397f9787bf30f9511b385c0bf4b1
sajeevan <sajeevanjspy@gmail.com>
2025-11-02 23:31:43 +0530

    test: add tests for /signout endpoint - added tests to verify /logout functionality - test ensures user can successfully log out when a valid session token is provided - added negative test case to handle missing or invalid session cookies - verified proper HTTP status codes and JSON response structure

--- api/src/routes/v1/auth/signout.rs
+++ api/src/routes/v1/auth/signout.rs
@@ -36,3 +36,77 @@ async fn signout(
         .cookie(expired_cookie)
         .json(ApiResult::<()>::success_message("logged out successfully")))
 }
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{App, test};
+
+    use super::*;
+
+    const JWT_SECRET: &'static str = "test_secret";
+    const JWT_ACCESS_EXPIRY_MINUTES: i64 = 20;
+    const JWT_REFRESH_EXPIRY_DAYS: i64 = 1;
+    const USER_ID_1: uuid::Uuid = uuid::Uuid::from_u128(1);
+
+    #[actix_web::test]
+    async fn test_signout_success() {
+        let jwt_service = JwtService::new(
+            JWT_SECRET.to_string(),
+            JWT_ACCESS_EXPIRY_MINUTES,
+            JWT_REFRESH_EXPIRY_DAYS,
+        );
+
+        let app = test::init_service(
+            App::new()
+                .app_data(web::Data::new(jwt_service.clone()))
+                .service(signout),
+        )
+        .await;
+
+        let (access_token, _) = jwt_service
+            .generate_tokens(USER_ID_1)
+            .expect("failed to generate tokens");
+
+        let req = test::TestRequest::post()
+            .uri("/signout")
+            .cookie(
+                actix_web::cookie::Cookie::build(JwtService::JWT_SESSION_KEY, access_token.clone())
+                    .http_only(true)
+                    .secure(false)
+                    .finish(),
+            )
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+        assert_eq!(resp.status(), 200);
+
+        let body: ApiResult<()> = test::read_body_json(resp).await;
+        assert!(body.success);
+        assert_eq!(body.message, Some("logged out successfully".to_string()));
+    }
+
+    #[actix_web::test]
+    async fn test_logout_without_cookie() {
+        let jwt_service = JwtService::new(
+            JWT_SECRET.to_string(),
+            JWT_ACCESS_EXPIRY_MINUTES,
+            JWT_REFRESH_EXPIRY_DAYS,
+        );
+
+        let app = test::init_service(
+            App::new()
+                .app_data(web::Data::new(jwt_service.clone()))
+                .service(signout),
+        )
+        .await;
+
+        let req = test::TestRequest::post().uri("/signout").to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert_eq!(resp.status(), 401);
+
+        let body: ApiResult<()> = test::read_body_json(resp).await;
+        assert!(!body.success);
+        assert_eq!(body.message, Some("user is not authenticated".to_string()));
+    }
+}
