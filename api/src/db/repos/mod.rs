329c232e298cd07d5906ba76a23f13bff402e1c8
sajeevan <sajeevanjspy@gmail.com>
2025-11-03 12:31:35 +0530

    feat: implement database access methods in OrgRepo - added OrgRepo for interacting with the organizations table - implemented CRUD and query functions for organization - encapsultes database logic for organization-related operations

--- api/src/db/repos/mod.rs
+++ api/src/db/repos/mod.rs
@@ -1,3 +1,5 @@
+mod org_repo;
 mod user_repo;
 
+pub use org_repo::OrgRepo;
 pub use user_repo::UserRepo;
---
+++ api/src/db/repos/org_repo.rs
@@ -0,0 +1,81 @@
+use sea_query::*;
+use sqlx::PgPool;
+
+use crate::db::models::Organization;
+
+const PG_TABLE_ORGS: &'static str = "organizations";
+
+pub struct OrgRepo<'a> {
+    pub pool: &'a PgPool,
+}
+
+impl<'a> OrgRepo<'a> {
+    pub fn new(pool: &'a PgPool) -> Self {
+        Self { pool }
+    }
+
+    pub async fn create_org(&self, org: &Organization) -> sqlx::Result<Organization> {
+        let query = Query::insert()
+            .into_table(Alias::new(PG_TABLE_ORGS))
+            .columns(["owner_id", "name", "subdomain"])
+            .values_panic([
+                org.owner_id.clone().to_string().into(),
+                org.name.clone().into(),
+                org.subdomain.clone().into(),
+            ])
+            .returning_all()
+            .to_string(PostgresQueryBuilder);
+
+        let inserted_org = sqlx::query_as::<_, Organization>(&query)
+            .fetch_one(self.pool)
+            .await?;
+
+        Ok(inserted_org)
+    }
+
+    pub async fn find_by_ownerid(&self, owner_id: uuid::Uuid) -> sqlx::Result<Vec<Organization>> {
+        let query = Query::select()
+            .column(Asterisk)
+            .from(PG_TABLE_ORGS)
+            .and_where(Expr::col(Alias::new("owner_id")).eq(owner_id.to_string()))
+            .to_string(PostgresQueryBuilder);
+
+        let org = sqlx::query_as::<_, Organization>(&query)
+            .fetch_all(self.pool)
+            .await?;
+        Ok(org)
+    }
+
+    pub async fn find_by_orgid(&self, org_id: uuid::Uuid) -> sqlx::Result<Option<Organization>> {
+        let query = Query::select()
+            .column(Asterisk)
+            .from(PG_TABLE_ORGS)
+            .and_where(Expr::col(Alias::new("organization_id")).eq(org_id.to_string()))
+            .to_string(PostgresQueryBuilder);
+
+        let org = sqlx::query_as::<_, Organization>(&query)
+            .fetch_optional(self.pool)
+            .await?;
+        Ok(org)
+    }
+
+    pub async fn delete_by_orgid(&self, org_id: uuid::Uuid) -> sqlx::Result<u64> {
+        let query = Query::delete()
+            .from_table(Alias::new(PG_TABLE_ORGS))
+            .and_where(Expr::col(Alias::new("organization_id")).eq(org_id.to_string()))
+            .to_string(PostgresQueryBuilder);
+
+        let result = sqlx::query(&query).execute(self.pool).await?;
+        Ok(result.rows_affected())
+    }
+
+    pub async fn delete_by_subdomain(&self, subdomain: String) -> sqlx::Result<u64> {
+        let query = Query::delete()
+            .from_table(Alias::new(PG_TABLE_ORGS))
+            .and_where(Expr::col(Alias::new("subdomain")).eq(subdomain))
+            .to_string(PostgresQueryBuilder);
+
+        let result = sqlx::query(&query).execute(self.pool).await?;
+        Ok(result.rows_affected())
+    }
+}
