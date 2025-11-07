2c338eb3a4c15fba6788af21e0b5b1739e4ebee6
sajeevan <sajeevanjspy@gmail.com>
2025-11-07 19:07:48 +0530

    chore: update ToastProvider configuration and behavior - adjusted toast position - added custom color variants for success and error states

--- apps/web/src/hooks/toast-provider.tsx
+++ apps/web/src/hooks/toast-provider.tsx
@@ -5,16 +5,23 @@ import { Toaster } from "@/components/ui/sonner";
 export function ToastProvider() {
     return (
         <Toaster
-            position="top-right"
+            position="top-center"
             toastOptions={{
+                duration: 3000,
                 classNames: {
-                    toast: "bg-zinc-900 text-zinc-100 border border-zinc-800 shadow-lg rounded-xl",
+                    toast: "bg-zinc-900 text-zinc-100 font-bold border border-zinc-800 shadow-lg rounded-xl",
+                    success: "bg-green-500",
+                    error: "bg-red-600",
+                    info: "bg-blue-500",
+                    warning: "bg-yellow-500 text-black",
                     title: "text-sm font-medium",
                     description: "text-xs text-zinc-400",
                     actionButton:
                         "bg-zinc-700 hover:bg-zinc-600 text-zinc-100 rounded-md px-2 py-1 text-xs",
                 },
             }}
+            richColors
+            closeButton
         />
     );
 }
